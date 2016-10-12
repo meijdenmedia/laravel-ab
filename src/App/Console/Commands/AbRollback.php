9a368a6879e55a542ebdfebf089ba82c2dc779f9
Joost van der Meijden <joostvandermeijden@live.nl>
2016-10-12 15:05:07 +0200

    Removed app folder

--- src/App/Console/Commands/AbRollback.php
+++ src/App/Console/Commands/AbRollback.php
@@ -22,45 +22,29 @@ class AbRollback extends Command
 
     /**
      * Create a new command instance.
-     *
-     * @return void
      */
     public function __construct()
     {
         parent::__construct();
-        
         include_once realpath(__DIR__.'/../../../../migrations/2015_08_15_000001_create_ab_tables.php');
-        include_once realpath(__DIR__.'/../../../../migrations/2016_10_12_000001_fix_ab_column_types.php');
     }
 
     /**
      * Execute the console command.
      *
-     * @return void
+     * @return mixed
      */
     public function handle()
     {
         if ($this->confirm('Do you wish to continue? [y|N]')) {
-            
-            // TODO: create a more efficient way to handle down migrations
-            
-            $fixMigration = new \FixAbColumnTypes();
-            if ($fixMigration->down()) {
-                $this->info("Column types reverted successfully");
+            $migration = new \CreateAbTables();
+            if ($migration->down()) {
+                $this->info('AB tables destroyed successfully');
             } else {
-                $this->error("Could not revert column types");
+                $this->error('Could not delete AB tables');
             }
-            
-            $create_migration = new \CreateAbTables();
-            if ($create_migration->down()) {
-                $this->info("AB tables destroyed successfully");
-            } else {
-                $this->error("Could not delete AB tables");
-            }
-        }
-        else {
-            $this->error("user exited, nothing done");
+        } else {
+            $this->error('user exited, nothing done');
         }
     }
 }
-
